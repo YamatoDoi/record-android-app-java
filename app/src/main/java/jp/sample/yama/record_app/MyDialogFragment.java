a3044d229184284b71b3245025e8367ba19638f3
YamatoDoi <doiyamato1994@gmail.com>
2025-11-06 13:10:09 +0900

    changed package_name

---
+++ app/src/main/java/jp/sample/yama/record_app/MyDialogFragment.java
@@ -0,0 +1,25 @@
+package jp.sample.yama.record_app;
+
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import jp.sample.yama.R;
+
+
+public class MyDialogFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        return new MaterialAlertDialogBuilder(requireActivity())
+                .setTitle(R.string.button_usage)
+                .setMessage(R.string.usage_body)
+                .create();
+    }
+}
